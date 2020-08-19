1c5743612777245f9f16c91718b5acc3f5d24e88
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-08-19 02:53:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,7 +62,7 @@ class Student
   end
   
   def self.first_student_in_grade_10
-    s = self.first_X_students_in_grade_10student_in_grade_10
+    s = self.first_X_students_in_grade_10
     s.collect do |x|
       self.new_from_db(x)
     end[0]
