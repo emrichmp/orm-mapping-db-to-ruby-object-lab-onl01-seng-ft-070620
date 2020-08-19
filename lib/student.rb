bf765f378b3e5003dfba0ed735c1b7aee09d0282
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-08-19 02:41:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,7 +22,11 @@ class Student
     SELECT *
     FROM students
     WHERE name = ?
-    LIMIT log10
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
   def save
